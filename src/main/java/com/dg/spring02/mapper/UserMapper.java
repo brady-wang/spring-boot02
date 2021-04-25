1b08813f94cc19fa9f7f4d5f86fd8fd8498c9948
wangys <brady.wang@qq.com>
2021-04-25 11:59:47 +0800

    f

--- src/main/java/com/dg/spring02/mapper/UserMapper.java
+++ src/main/java/com/dg/spring02/mapper/UserMapper.java
@@ -10,4 +10,9 @@ import org.springframework.stereotype.Repository;
 public interface UserMapper {
 
     User Sel(int id);
+
+    User login(String userName,String passWord);
+
+    int register(User user);
+
 }
--- src/main/java/com/dg/spring02/service/UserService.java
+++ src/main/java/com/dg/spring02/service/UserService.java
@@ -6,16 +6,20 @@ import com.dg.spring02.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * @Author:wjup
- * @Date: 2018/9/26 0026
- * @Time: 15:23
- */
+
 @Service
 public class UserService {
     @Autowired
     UserMapper userMapper;
     public User Sel(int id){
         return userMapper.Sel(id);
+    }
+
+    public User login(String userName, String passWord) {
+        return userMapper.login(userName,passWord);
+    }
+
+    public int register(User user) {
+        return userMapper.register(user);
     }
 }
